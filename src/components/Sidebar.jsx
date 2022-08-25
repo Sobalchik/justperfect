564aba95b15e479a38b4ba4013e5f18ffeb37b4a
Dmitrii Sobinin <Sobalchik@yandex.ru>
2022-08-25 20:24:44 +0200

    Появилась визучальная часть поста

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -18,7 +18,7 @@ const Sidebar = ({ closeToggle, user }) => {
             <div className="flex flex-col">
                 <Link
                     to="/"
-                    className="flex px-5 gap-2 my-6 pt-1 w-190 items-center"
+                    className="flex px-5 gap-2 my-6 w-190 items-center"
                     onClick={handleCloseSidebar}
                 >
                     <img src={logo} alt="logo" className="w-full" />
